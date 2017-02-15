adb778536360c95f6290bba08f967fbd42985ecb
Jim McCusker <mccusker@gmail.com>
2017-02-15 10:57:05 -0500

    forgot to allow for redirects

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://downloads.sourceforge.net/project/bigdata/bigdata/2.1.1/blazegraph.jar > blazegraph.jar
+curl -L http://downloads.sourceforge.net/project/bigdata/bigdata/2.1.1/blazegraph.jar > blazegraph.jar
 virtualenv --no-site-packages venv
 source venv/bin/activate
 pip install -r requirements/dev.txt
@@ -11,5 +11,6 @@ echo "  > java -jar blazegraph.jar &"
 echo ""
 echo "to start the RDF database (Blazegraph)."
 echo "And then run the following to start graphene:"
+echo ""
 echo "  > source venv/bin/activate"
 echo "  > python manage.py runserver"
