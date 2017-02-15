e618ee8c76db39063d65d73e41f6bf2cbec6f97a
Jim McCusker <mccusker@gmail.com>
2017-02-15 10:54:00 -0500

    Added a setup script to make it easier to run.

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl http://downloads.sourceforge.net/project/bigdata/bigdata/2.1.1/blazegraph.jar > blazegraph.jar
+virtualenv --no-site-packages venv
+source venv/bin/activate
+pip install -r requirements/dev.txt
+
+echo "To run graphene in development mode, run:"
+echo ""
+echo "  > java -jar blazegraph.jar &"
+echo ""
+echo "to start the RDF database (Blazegraph)."
+echo "And then run the following to start graphene:"
+echo "  > source venv/bin/activate"
+echo "  > python manage.py runserver"
