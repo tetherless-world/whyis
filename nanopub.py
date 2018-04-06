d997eca94b39ee80f0ca975e54fb8655290abcec
Jim McCusker <mccusker@gmail.com>
2018-04-06 16:51:15 +0000

    Added basic graph exploration, including incoming and outgoing links and deleting nodes.

--- nanopub.py
+++ nanopub.py
@@ -12,8 +12,6 @@ from depot.manager import DepotManager
 from datetime import datetime
 import pytz
 
-from memory_profiler import memory_usage
-
 np = rdflib.Namespace("http://www.nanopub.org/nschema#")
 prov = rdflib.Namespace("http://www.w3.org/ns/prov#")
 dc = rdflib.Namespace("http://purl.org/dc/terms/")
@@ -182,7 +180,6 @@ class NanopublicationManager:
                 nanopub.pubinfo.add((nanopub.identifier, rdflib.RDF.type, frbr.Expression))
             #print "Total", len(output_graph)
             #print "Contexts", [g.identifier for g in output_graph.contexts()]
-            print "Nanopub %s at %s MB" % (i, memory_usage()[0])
             yield nanopub
             
     def retire(self, *nanopub_uris):
@@ -253,7 +250,7 @@ class NanopublicationManager:
             self.retire(*to_retire)
             data.seek(0)
             self.db.store.publish(data, *nanopubs)
-            print "Published %s nanopubs %s MB" % (len(nanopubs), memory_usage()[0])
+            print "Published %s nanopubs" % len(nanopubs)
             
         for nanopub in nanopubs:
             self.update_listener(nanopub.identifier)
