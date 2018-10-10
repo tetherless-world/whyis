f7d9e510e754fa9a18975b036d19c50994f2768e
Jim McCusker <mccusker@gmail.com>
2018-10-10 23:14:03 +0000

    Fixed Ontology importer and added unit tests for some ontologies we use a lot.

--- autonomic.py
+++ autonomic.py
@@ -256,17 +256,22 @@ class OntologyImporter(UpdateChangeService):
 
     def process_nanopub(self, i, o, new_np):
         if (i.identifier, rdflib.RDF.type, whyis.ImportedOntology) in self.app.db:
+            print "Skipping already imported ontology:", i.identifier
             return
+        print "Attempting to import", i.identifier
         file_format = rdflib.util.guess_format(i.identifier)
         try: # Try the best guess at a format.
             g = rdflib.Graph()
             g.parse(location=i.identifier, format=file_format, publicID=self.app.NS.local)
             g.serialize() # to test that parsing was actually successful.
-            new_np.assertion.addN([(s,p,o,new_np.assertion.identifier) for s,p,o in g])
+            for s,p,o in g:
+                new_np.assertion.add((s,p,o))
             logging.debug("%s was parsed as %s"%(i.identifier,file_format))
+            print "%s was parsed as %s"%(i.identifier,file_format)
+            print len(new_np.assertion), len(g)
         except Exception: # If that doesn't work, brute force it with all possible RDF formats, most likely first.
             print "Could not parse %s as %s" %(i.identifier,file_format)
-            traceback.print_exc(file=sys.stdout)
+            #traceback.print_exc(file=sys.stdout)
             parsed = False
             for f in ['xml', 'turtle', 'trig', # Most likely
                       'n3','nquads','nt', # rarely used for ontologies, but sometimes
@@ -275,16 +280,19 @@ class OntologyImporter(UpdateChangeService):
                       'rdfa1.1','rdfa1.0','rdfa', # rare, but I've seen them.
                       'mdata','microdata','html']: # wow, there are a lot of RDF formats...
                 try:
+                    #print "Trying", f
                     g = rdflib.Graph()
                     g.parse(location=i.identifier, format=f, publicID=self.app.NS.local)
                     g.serialize() # to test that parsing was actually successful.
-                    new_np.assertion.addN([(s,p,o,new_np.assertion.identifier) for s,p,o in g])
+                    for s,p,o in g:
+                        new_np.assertion.add((s,p,o))
                     logging.debug("%s was parsed as %s"%(i.identifier, f))
+                    print "%s was parsed as %s"%(i.identifier, f)
                     parsed = True
                     break
                 except Exception:
                     print "BF: Could not parse %s as %s" %(i.identifier,f)
-                    traceback.print_exc(file=sys.stdout)
+                    #traceback.print_exc(file=sys.stdout)
                     pass
             if not parsed: # probably the best guess anyways, retry to throw the best possible exception.
                 print "Could not import ontology %s" % i.identifier
---
+++ tests/test_agents.py
@@ -0,0 +1,67 @@
+from testcase import WhyisTestCase
+
+from base64 import b64encode
+
+from rdflib import *
+
+import json
+from StringIO import StringIO
+
+import nanopub
+
+import autonomic
+
+class OntologyImportAgentTestCase(WhyisTestCase):
+
+    def test_foaf_import(self):
+        np = nanopub.Nanopublication()
+        np.assertion.parse(data='''{
+         "@id": "http://example.com/testonto",
+         "@type" : "http://www.w3.org/2002/07/owl#Ontology",
+         "http://www.w3.org/2002/07/owl#imports":{"@id":"http://xmlns.com/foaf/0.1/"}
+        }''', format="json-ld")
+        agent = autonomic.OntologyImporter()
+
+        results = self.run_agent(agent, nanopublication=np)
+        self.assertEquals(len(results), 1)
+        self.assertTrue(results[0].resource(URIRef('http://xmlns.com/foaf/0.1/'))[RDF.type:OWL.Ontology])
+
+    def test_dc_terms_import(self):
+        np = nanopub.Nanopublication()
+        np.assertion.parse(data='''{
+         "@id": "http://example.com/testonto",
+         "@type" : "http://www.w3.org/2002/07/owl#Ontology",
+         "http://www.w3.org/2002/07/owl#imports":{"@id":"http://purl.org/dc/terms/"}
+        }''', format="json-ld")
+        agent = autonomic.OntologyImporter()
+
+        results = self.run_agent(agent, nanopublication=np)
+        self.assertEquals(len(results), 1)
+        print results[0].serialize(format="trig")
+        self.assertTrue(results[0].resource(URIRef('http://purl.org/dc/terms/created'))[RDF.type:RDF.Property])
+
+    def test_hasco_import(self):
+        np = nanopub.Nanopublication()
+        np.assertion.parse(data='''{
+         "@id": "http://example.com/testonto",
+         "@type" : "http://www.w3.org/2002/07/owl#Ontology",
+         "http://www.w3.org/2002/07/owl#imports":{"@id":"http://hadatac.org/ont/hasco/"}
+        }''', format="json-ld")
+        agent = autonomic.OntologyImporter()
+
+        results = self.run_agent(agent, nanopublication=np)
+        self.assertEquals(len(results), 1)
+        self.assertTrue(results[0].resource(URIRef('http://hadatac.org/ont/hasco/'))[RDF.type:OWL.Ontology])
+
+    def test_sio_import(self):
+        np = nanopub.Nanopublication()
+        np.assertion.parse(data='''{
+         "@id": "http://example.com/testonto",
+         "@type" : "http://www.w3.org/2002/07/owl#Ontology",
+         "http://www.w3.org/2002/07/owl#imports":{"@id":"http://semanticscience.org/ontology/sio.owl"}
+        }''', format="json-ld")
+        agent = autonomic.OntologyImporter()
+
+        results = self.run_agent(agent, nanopublication=np)
+        self.assertEquals(len(results), 1)
+        self.assertTrue(results[0].resource(URIRef('http://semanticscience.org/ontology/sio.owl'))[RDF.type:OWL.Ontology])
