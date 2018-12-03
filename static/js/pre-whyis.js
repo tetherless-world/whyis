3e8c174bf8ef167463d31b6613a7a0ff8ba99d52
Henrique O. Santos <hensantos@yahoo.com>
2018-12-03 15:39:34 -0500

    dsa code split

---
+++ static/js/pre-whyis.js
@@ -0,0 +1,4 @@
+$( function() {
+    var app = angular.module('App', ['ngSanitize', 'ngMaterial', 'lfNgMdFileInput', 'ui.bootstrap', 'seco.facetedSearch','jsonLdEditor']);
+    console.log("Here's the app at pre-whyis.js",app);
+});
