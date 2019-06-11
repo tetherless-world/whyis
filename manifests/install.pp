5d6344bde97e925ec7d228acce2cb06597088506
Jim McCusker <mccusker@gmail.com>
2019-06-11 13:25:25 -0400

    Added more needed packages.

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,7 +8,7 @@ class { 'python' :
   gunicorn   => 'absent',
 }
 
-package { ["unzip", "zip", "default-jdk", "build-essential","automake", "jetty8", "subversion", "git", "libapache2-mod-wsgi", "libblas3", "libblas-dev", "celeryd", "redis-server", "apache2", "libffi-dev", "libssl-dev", "maven"]:
+package { ["unzip", "zip", "default-jdk", "build-essential","automake", "jetty8", "subversion", "git", "libapache2-mod-wsgi", "libblas3", "libblas-dev", "celeryd", "redis-server", "apache2", "libffi-dev", "libssl-dev", "maven", "python3-dev", "libdb5.3-dev"]:
   ensure => "installed"
 } ->
 file_line { "configure_jetty_start":
