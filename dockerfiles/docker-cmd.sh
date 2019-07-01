9d572b2b5708d1396488e92ea3a6ee439c95c3b8
Minor Gordon <gordom6@rpi.edu>
2019-07-01 16:16:46 -0400

    working on Dockerfile

--- dockerfiles/docker-cmd.sh
+++ dockerfiles/docker-cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# service jetty9 stop
-# rm -f /var/run/jetty9.pid
+#service apache2 start
+#service celeryd start
 service jetty9 start
 
 if [ ! -f ".data_loaded" ] ;
@@ -10,10 +10,23 @@ then
 
     echo "Loading data"
     curl -X POST --data-binary "@admin.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/admin_namespace.log &&\
-	curl -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log
-	echo "Loaded data"
+    curl -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log
+    echo "Loaded data"
 
     echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .data_loaded
 fi
+
+
+echo "\n\
+Please configure Whyis at /apps/whyis/config.py to ensure correct customization.\n\
+Follow the instructions for 'Configure Whyis' at http://tetherless-world.github.io/whyis/install.\n\
+\n\
+To run whyis in development mode, run the following to start it:\n\
+\n\
+ > sudo su - whyis\n\
+ > cd /apps/whyis\n\
+ > source venv/bin/activate\n\
+ > python manage.py runserver\n\
+\n"
 
 /bin/bash
