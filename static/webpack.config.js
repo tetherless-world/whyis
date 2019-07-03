5ce0e0c19ed7eaf68c03643e733c413a4c84dd9b
Minor Gordon <gordom6@rpi.edu>
2019-07-03 11:08:59 -0400

    minimalist webpack.config.js

---
+++ static/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = (env, argv) => ({
+    devServer: {
+        contentBase: ".",
+        host: "localhost",
+        hot: true,
+        inline: true,
+        port: 9000,
+        proxy: {
+            "/api": {
+                "target": {
+                    "host": "localhost",
+                    "protocol": 'http:',
+                    "port": 5000
+                },
+                secure: false
+            }
+        }
+    },
+    devtool: argv.mode === "production" ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+    entry: {
+        app: ['./js/whyis_vue/main.js']
+    },
+    externals: {
+        "jquery": "jQuery"
+    },
+    module: {
+        rules: [{
+            test: /(\.css$)/,
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
+        }]
+    },
+    name: "whyis",
+    output: {
+        filename: 'js/whyis_vue_bundle.js',
+        libraryTarget: 'umd',
+        path: __dirname
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/whyis_vue_bundle.css"
+        })
+    ],
+    resolve: {
+        extensions: ['.js'],
+        modules: [path.join(__dirname, 'js', 'whyis_vue'), path.join(__dirname, 'node_modules')]
+    },
+    resolveLoader: {
+        modules: [path.join(__dirname, 'node_modules')]
+    },
+    target: "web"
+});
