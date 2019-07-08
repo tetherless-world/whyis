06cc67dd4188ac73555bb602c0d9c81b78692cd8
Minor Gordon <gordom6@rpi.edu>
2019-07-08 15:55:58 -0400

    add python3-pip

--- docker/image/whyis-deps/install.pp
+++ docker/image/whyis-deps/install.pp
@@ -14,7 +14,7 @@ service { jetty8:
 
 }
 
-package { ["unzip", "zip", "default-jdk", "build-essential","automake", "jetty9", "subversion", "git", "libapache2-mod-wsgi", "libblas3", "libblas-dev", "celeryd", "redis-server", "apache2", "libffi-dev", "libssl-dev", "maven", "python3-dev", "libdb5.3-dev"]:
+package { ["unzip", "zip", "default-jdk", "build-essential","automake", "jetty9", "subversion", "git", "libapache2-mod-wsgi", "libblas3", "libblas-dev", "celeryd", "redis-server", "apache2", "libffi-dev", "libssl-dev", "maven", "python3-dev", "python3-pip", "libdb5.3-dev"]:
   ensure => "installed"
 } ->
 package { "jetty8":
