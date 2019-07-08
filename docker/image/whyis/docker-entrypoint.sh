236387fea58fdcd4ce220d29e330739dbe662853
Minor Gordon <gordom6@rpi.edu>
2019-07-08 16:38:18 -0400

    log docker-entrypoint.sh output instead of redirecting to /dev/null

--- docker/image/whyis/docker-entrypoint.sh
+++ docker/image/whyis/docker-entrypoint.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-service apache2 start &>/dev/null
-service jetty9 start &>/dev/null
-service redis-server start &>/dev/null
-service celeryd start &>/dev/null
+service apache2 start &>docker-entrypoint.log
+service jetty9 start &>>docker-entrypoint.log
+service redis-server start &>docker-entrypoint.log
+service celeryd start &>docker-entrypoint.log
 
 if [ ! -f ".data_loaded" ] ;
 then
     for i in 1 2 3 4 5; do curl -s http://localhost:8080 &>/dev/null && break || sleep 1; done
 
-    curl -s -X POST --data-binary "@admin.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/admin_namespace.log 2>/dev/null &&\
-    curl -s -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log 2>/dev/null
+    curl -s -X POST --data-binary "@admin.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/admin_namespace.log 2>/docker-entrypoint.log &&\
+    curl -s -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log 2>/docker-entrypoint.log
 
     echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .data_loaded
 fi
-
-
-#echo "\n\
-#Please configure Whyis at /apps/whyis/config.py to ensure correct customization.\n\
-#Follow the instructions for 'Configure Whyis' at http://tetherless-world.github.io/whyis/install.\n\
-#\n\
-#To run whyis in development mode, run the following to start it:\n\
-#\n\
-# > sudo su - whyis\n\
-# > cd /apps/whyis\n\
-# > source venv/bin/activate\n\
-# > python manage.py runserver\n\
-#\n"
 
 exec "$@"
