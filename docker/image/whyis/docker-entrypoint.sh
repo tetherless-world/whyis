0356ff4c114100b279a3bf97e1aea0619cb180e1
Minor Gordon <gordom6@rpi.edu>
2019-07-09 14:20:02 -0400

    redirect stdout to stderr instead of to a file

--- docker/image/whyis/docker-entrypoint.sh
+++ docker/image/whyis/docker-entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-service apache2 start &>>/docker-entrypoint.log
-service jetty9 start &>>/docker-entrypoint.log
-service redis-server start &>/docker-entrypoint.log
-service celeryd start &>/docker-entrypoint.log
+# jetty9 service start returns 1, even though Jetty has started successfully.
+# set -e
 
-if [ ! -f ".data_loaded" ] ;
+service apache2 start 1>&2
+service jetty9 start 1>&2
+service redis-server start 1>&2
+service celeryd start 1>&2
+
+if [ ! -f "/data/.whyis_data_loaded" ] ;
 then
-    for i in 1 2 3 4 5; do curl -s http://localhost:8080 &>/dev/null && break || sleep 1; done
+    for i in 1 2 3 4 5; do curl -s http://localhost:8080 1>&2 && break || sleep 1; done
 
-    curl -s -X POST --data-binary "@admin.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/admin_namespace.log 2>>/docker-entrypoint.log &&\
-    curl -s -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log 2>>/docker-entrypoint.log
+    curl -s -X POST --data-binary "@admin.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/admin_namespace.log
+    curl -s -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log
 
-    echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .data_loaded
+    echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > /data/.whyis_data_loaded
 fi
 
 exec "$@"
