75dd4394371e8ea5317962a99a646f3ab2ba724f
Minor Gordon <gordom6@rpi.edu>
2019-07-09 14:23:05 -0400

    don't need to print test calls

--- docker/image/whyis/docker-entrypoint.sh
+++ docker/image/whyis/docker-entrypoint.sh
@@ -10,7 +10,7 @@ service celeryd start 1>&2
 
 if [ ! -f "/data/.whyis_data_loaded" ] ;
 then
-    for i in 1 2 3 4 5; do curl -s http://localhost:8080 1>&2 && break || sleep 1; done
+    for i in 1 2 3 4 5; do curl -s http://localhost:8080 &>/dev/null && break || sleep 1; done
 
     curl -s -X POST --data-binary "@admin.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/admin_namespace.log
     curl -s -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log
