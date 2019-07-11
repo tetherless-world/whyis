e3348f8fe4d48aaf07d8685f9356dd26fbb475be
Owen Xie <owenxie24@gmail.com>
2019-07-11 17:51:12 -0400

    Removed celery container

--- docker/image/whyis-server/docker-entrypoint.sh
+++ docker/image/whyis-server/docker-entrypoint.sh
@@ -3,15 +3,16 @@
 # jetty9 service start returns 1, even though Jetty has started successfully.
 # set -e
 
-socat TCP4-LISTEN:6379,fork,reuseaddr TCP4:redis:6379 
-socat TCP4-LISTEN:8080,fork,reuseaddr TCP4:blazegraph:8080
+for i in 1 2 3 4 5; do curl -s http://blazegraph:8080 &>/dev/null && break || sleep 1; done
+
+socat TCP4-LISTEN:6379,fork,reuseaddr TCP4:redis:6379 &
+socat TCP4-LISTEN:8080,fork,reuseaddr TCP4:blazegraph:8080 &
 
 service apache2 start 1>&2
+service celeryd start 1>&2
 
 if [ ! -f "/data/.whyis_data_loaded" ] ;
 then
-    curl -s --retry 5 http://localhost:8080 &>/dev/null
-
     curl -s -X POST --data-binary "@admin.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/admin_namespace.log
     curl -s -X POST --data-binary "@knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log
 
