c79ab010b85477b37015a2258b59123cfcad213e
Minor Gordon <gordom6@rpi.edu>
2019-07-12 10:50:57 -0400

    get_max_id not used anywhere

--- utils.py
+++ utils.py
@@ -8,17 +8,3 @@ from datetime import datetime
 
 def create_id():
     return base64.encodestring(str(random.random()*datetime.now().toordinal()).encode('utf8')).decode('utf8').rstrip('=\n')
-
-def get_max_id(c, graph):
-    query = '''select ?id where { ?s a %s ;
-               <http://purl.org/dc/terms/identifier> ?id.
-               } order by desc(?id) limit 1''' % c.n3()
-    i = list(graph.query(query))
-    if len(i) == 0:
-        return 0
-    else:
-        result = i[0][0].value
-        if result < 1:
-            return Literal(0)
-        else:
-            return Literal(result)
