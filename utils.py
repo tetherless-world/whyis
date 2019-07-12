1618c6526d595e673732e2ea606fb37f52b348bc
Minor Gordon <gordom6@rpi.edu>
2019-07-12 10:49:59 -0400

    utils.slugify not used, python-slugify used everywhere

--- utils.py
+++ utils.py
@@ -31,19 +31,3 @@ def get_max_id(c, graph):
             return Literal(0)
         else:
             return Literal(result)
-
-_slugify_strip_re = re.compile(r'[^\w\s-]')
-_slugify_hyphenate_re = re.compile(r'[-\s]+')
-def slugify(value):
-    """
-    Normalizes string, converts to lowercase, removes non-alpha characters,
-    and converts spaces to hyphens.
-    
-    From Django's "django/template/defaultfilters.py".
-    """
-    import unicodedata
-    if not isinstance(value, str):
-        value = str(value)
-    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
-    value = str(_slugify_strip_re.sub('', value).strip().lower())
-    return _slugify_hyphenate_re.sub('-', value)
