6453351d4b047e09bb0736e079012013d01ab719
Minor Gordon <gordom6@rpi.edu>
2019-07-19 14:31:56 -0400

    pep8

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -3,6 +3,7 @@
 mkdir -p lint-results/py
 
 flake8 . &>lint-results/py/flake8.txt
+pep8 . &>lint-results/py/pep8.txt
 
 cd ..
 pylint --rcfile whyis/.pylintrc whyis &>whyis/lint-results/py/pylint.txt
