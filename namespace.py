bf3c0c086989880917db9e2233e11abf14fabfec
Minor Gordon <gordom6@rpi.edu>
2019-07-22 16:48:20 -0400

    organize namespace.py

--- namespace.py
+++ namespace.py
@@ -1,10 +1,12 @@
 import rdflib
 
-whyis = rdflib.Namespace('http://vocab.rpi.edu/whyis/')
+dc = rdflib.Namespace("http://purl.org/dc/terms/")
 np = rdflib.Namespace("http://www.nanopub.org/nschema#")
 prov = rdflib.Namespace("http://www.w3.org/ns/prov#")
-dc = rdflib.Namespace("http://purl.org/dc/terms/")
-sio = rdflib.Namespace("http://semanticscience.org/resource/")
-setl = rdflib.Namespace("http://purl.org/twc/vocab/setl/")
 pv = rdflib.Namespace("http://purl.org/net/provenance/ns#")
+setl = rdflib.Namespace("http://purl.org/twc/vocab/setl/")
+sio = rdflib.Namespace("http://semanticscience.org/resource/")
+sioc = rdflib.Namespace("http://rdfs.org/sioc/ns#")
+sioc_types = rdflib.Namespace("http://rdfs.org/sioc/types#")
 skos = rdflib.Namespace("http://www.w3.org/2008/05/skos#")
+whyis = rdflib.Namespace('http://vocab.rpi.edu/whyis/')
