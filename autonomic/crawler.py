fe979912759a9843d3c5656124fcb4e6424af984
Minor Gordon <gordom6@rpi.edu>
2019-07-22 16:33:02 -0400

    autonomic: common namespaces file

--- autonomic/crawler.py
+++ autonomic/crawler.py
@@ -21,15 +21,7 @@ import tempfile
 
 from depot.io.interfaces import StoredFile
 
-whyis = rdflib.Namespace('http://vocab.rpi.edu/whyis/')
-whyis = rdflib.Namespace('http://vocab.rpi.edu/whyis/')
-np = rdflib.Namespace("http://www.nanopub.org/nschema#")
-prov = rdflib.Namespace("http://www.w3.org/ns/prov#")
-dc = rdflib.Namespace("http://purl.org/dc/terms/")
-sio = rdflib.Namespace("http://semanticscience.org/resource/")
-setl = rdflib.Namespace("http://purl.org/twc/vocab/setl/")
-pv = rdflib.Namespace("http://purl.org/net/provenance/ns#")
-skos = rdflib.Namespace("http://www.w3.org/2008/05/skos#")
+from .namespaces import *
 
 
 class Crawler(UpdateChangeService):
