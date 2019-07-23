41fdd897ba8440ca4c831ae6d773d17d06bc2d91
Minor Gordon <gordom6@rpi.edu>
2019-07-23 16:26:58 -0400

    enable vulture

--- .circleci/lint-py.sh
+++ .circleci/lint-py.sh
@@ -4,6 +4,7 @@ mkdir -p lint-results/py
 
 flake8 . &>lint-results/py/flake8.txt
 pycodestyle . &>lint-results/py/pycodestyle.txt
+vulture --exclude config-template,tests,venv . &>lint-results/py/vulture.txt
 
 cd ..
 pylint --rcfile whyis/.pylintrc whyis &>whyis/lint-results/py/pylint.txt
