2a446f2f173c01823bd4bbc55b2916ed2f02fd48
Minor Gordon <gordom6@rpi.edu>
2019-07-23 13:41:15 -0400

    use class properties

--- main.py
+++ main.py
@@ -70,6 +70,35 @@ top_compare_key = False, -100, [(-2, 0)]
 bottom_compare_key = True, 100, [(2, 0)]
 
 class NamespaceContainer(object):
+    RDFS = rdflib.RDFS
+    RDF = rdflib.RDF
+    rdfs = rdflib.Namespace(rdflib.RDFS)
+    rdf = rdflib.Namespace(rdflib.RDF)
+    owl = rdflib.OWL
+    xsd = rdflib.Namespace("http://www.w3.org/2001/XMLSchema#")
+    dc = rdflib.Namespace("http://purl.org/dc/terms/")
+    dcelements = rdflib.Namespace("http://purl.org/dc/elements/1.1/")
+    auth = rdflib.Namespace("http://vocab.rpi.edu/auth/")
+    foaf = rdflib.Namespace("http://xmlns.com/foaf/0.1/")
+    prov = rdflib.Namespace("http://www.w3.org/ns/prov#")
+    skos = rdflib.Namespace("http://www.w3.org/2004/02/skos/core#")
+    cmo = rdflib.Namespace("http://purl.org/twc/ontologies/cmo.owl#")
+    sio = rdflib.Namespace("http://semanticscience.org/resource/")
+    sioc_types = rdflib.Namespace("http://rdfs.org/sioc/types#")
+    sioc = rdflib.Namespace("http://rdfs.org/sioc/ns#")
+    np = rdflib.Namespace("http://www.nanopub.org/nschema#")
+    whyis = rdflib.Namespace("http://vocab.rpi.edu/whyis/")
+    ov = rdflib.Namespace("http://open.vocab.org/terms/")
+    frbr = rdflib.Namespace("http://purl.org/vocab/frbr/core#")
+    mediaTypes = rdflib.Namespace("https://www.iana.org/assignments/media-types/")
+    pv = rdflib.Namespace("http://purl.org/net/provenance/ns#")
+    sd = rdflib.Namespace('http://www.w3.org/ns/sparql-service-description#')
+    ld = rdflib.Namespace('http://purl.org/linked-data/api/vocab#')
+    dcat = rdflib.Namespace("http://www.w3.org/ns/dcat#")
+    hint = rdflib.Namespace("http://www.bigdata.com/queryHints#")
+    void = rdflib.Namespace("http://rdfs.org/ns/void#")
+    schema = rdflib.Namespace("http://schema.org/")
+
     @property
     def prefixes(self):
         result = {}
@@ -79,35 +108,7 @@ class NamespaceContainer(object):
         return result
 
 NS = NamespaceContainer()
-NS.RDFS = rdflib.RDFS
-NS.RDF = rdflib.RDF
-NS.rdfs = rdflib.Namespace(rdflib.RDFS)
-NS.rdf = rdflib.Namespace(rdflib.RDF)
-NS.owl = rdflib.OWL
-NS.xsd   = rdflib.Namespace("http://www.w3.org/2001/XMLSchema#")
-NS.dc    = rdflib.Namespace("http://purl.org/dc/terms/")
-NS.dcelements    = rdflib.Namespace("http://purl.org/dc/elements/1.1/")
-NS.auth  = rdflib.Namespace("http://vocab.rpi.edu/auth/")
-NS.foaf  = rdflib.Namespace("http://xmlns.com/foaf/0.1/")
-NS.prov  = rdflib.Namespace("http://www.w3.org/ns/prov#")
-NS.skos = rdflib.Namespace("http://www.w3.org/2004/02/skos/core#")
-NS.cmo = rdflib.Namespace("http://purl.org/twc/ontologies/cmo.owl#")
-NS.sio = rdflib.Namespace("http://semanticscience.org/resource/")
-NS.sioc_types = rdflib.Namespace("http://rdfs.org/sioc/types#")
-NS.sioc = rdflib.Namespace("http://rdfs.org/sioc/ns#")
-NS.np = rdflib.Namespace("http://www.nanopub.org/nschema#")
-NS.whyis = rdflib.Namespace("http://vocab.rpi.edu/whyis/")
-NS.ov = rdflib.Namespace("http://open.vocab.org/terms/")
-NS.frbr = rdflib.Namespace("http://purl.org/vocab/frbr/core#")
-NS.mediaTypes = rdflib.Namespace("https://www.iana.org/assignments/media-types/")
-NS.pv = rdflib.Namespace("http://purl.org/net/provenance/ns#")
-NS.sd = rdflib.Namespace('http://www.w3.org/ns/sparql-service-description#')
-NS.ld = rdflib.Namespace('http://purl.org/linked-data/api/vocab#')
-NS.dcat = rdflib.Namespace("http://www.w3.org/ns/dcat#")
-NS.hint = rdflib.Namespace("http://www.bigdata.com/queryHints#")
-NS.void = rdflib.Namespace("http://rdfs.org/ns/void#")
-NS.schema = rdflib.Namespace("http://schema.org/")
-    
+
 from datastore import *
 
 #from flask_login.config import EXEMPT_METHODS
