ea4fd64a7882ae48164ca69359613eb73975d3cb
Minor Gordon <gordom6@rpi.edu>
2019-07-26 10:35:02 -0400

    neglected to install coverage in the Circle config

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -5,20 +5,20 @@ WHYIS_IMAGE="${1:-tetherlessworld/whyis:latest}"
 mkdir -p test-results/py
 
 # docker run $WHYIS_IMAGE bash -c "python3 manage.py test"
+# docker run $WHYIS_IMAGE bash -c "python3 manage.py test --ci && cat test-results/py/results.xml"
 
-#docker run $WHYIS_IMAGE bash -c "python3 manage.py test --ci &>/dev/null && tar cf test-results-py.tar test-results/py && cat test-results-py.tar" >test-results-py.tar
-docker run $WHYIS_IMAGE bash -c "python3 manage.py test --ci && cat test-results/py/results.xml"
+docker run $WHYIS_IMAGE bash -c "python3 manage.py test --ci &>/dev/null && tar cf test-results-py.tar test-results/py && cat test-results-py.tar" >test-results-py.tar
 
-#tar xf test-results-py.tar
-#
-#if [ ! -f "test-results/py/results.xml" ]; then
-#    exit 1
-#fi
-#
-#cat test-results/py/results.xml
-#
-#if [ "$(grep -c "failure "test-results/py/results.xml)" -ge 1 ]; then
-#    exit 1
-#else
-#    exit 0
-#fi
+tar xf test-results-py.tar
+
+if [ ! -f "test-results/py/results.xml" ]; then
+    exit 1
+fi
+
+cat test-results/py/results.xml
+
+if [ "$(grep -c "failure "test-results/py/results.xml)" -ge 1 ]; then
+    exit 1
+else
+    exit 0
+fi
