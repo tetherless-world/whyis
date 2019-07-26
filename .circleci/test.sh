22e7c3e1fda48497e5561cae03bb9a43f55271de
Minor Gordon <gordom6@rpi.edu>
2019-07-26 09:21:43 -0400

    unit test check

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+WHYIS_IMAGE="${1:-tetherlessworld/whyis:latest}"
+
 mkdir -p test-results/py
 
-docker run $1 bash -c "python3 manage.py test --ci &>/dev/null && tar cf test-results-py.tar test-results/py && cat test-results-py.tar" >test-results-py.tar
+docker run $WHYIS_IMAGE bash -c "python3 manage.py test --ci &>/dev/null && tar cf test-results-py.tar test-results/py && cat test-results-py.tar" >test-results-py.tar
 
 tar xf test-results-py.tar
+
+if [ ! -f "test-results/py/results.xml" ]; then
+    exit 1
+fi
 
 cat test-results/py/results.xml
 
