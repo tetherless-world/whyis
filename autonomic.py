48c5783deead7cf68baca3336aafa2c9f6dbae1c
Minor Gordon <gordom6@rpi.edu>
2019-07-26 08:49:02 -0400

    deprecation warnings

--- autonomic.py
+++ autonomic.py
@@ -1,2 +1,5 @@
-from whyis.autonomic import *
+import warnings as __warnings
 
+__warnings.warn("unqualified module names deprecated, use whyis.autonomic", DeprecationWarning)
+
+from whyis.autonomic import *
--- commands.py
+++ commands.py
@@ -1,1 +1,5 @@
+import warnings as __warnings
+
+__warnings.warn("unqualified module names deprecated, use whyis.commands", DeprecationWarning)
+
 from whyis.commands import *
