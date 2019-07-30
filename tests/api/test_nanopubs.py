4d2abf00eed7b3b2566a3cfbaf9c3597872f7fd9
Owen Xie <owenxie24@gmail.com>
2019-07-30 12:42:48 -0400

    Add publish_nanopub and get_view helper methods to ApiTestCase

--- tests/api/test_nanopubs.py
+++ tests/api/test_nanopubs.py
@@ -1,27 +1,22 @@
-from rdflib import *
 
 import json
-
+from rdflib import ConjunctiveGraph, Graph, URIRef
+from rdflib.namespace import RDF
 from whyis.test.api_test_case import ApiTestCase
 
 
 class NanopubTest(ApiTestCase):
 
-    turtle  = '''
+    turtle = '''
 <http://example.com/janedoe> <http://schema.org/jobTitle> "Professor";
     <http://schema.org/name> "Jane Doe" ;
     <http://schema.org/telephone> "(425) 123-4567" ;
     <http://schema.org/url> <http://www.janedoe.com> ;
     <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .
 '''
-    
+
     def test_create(self):
-        self.login(*self.create_user("user@example.com","password"))
-
-        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle",follow_redirects=True)
-        
-        self.assertEquals(response.status,'201 CREATED')
-
+        response = self.publish_nanopub(self.turtle, "text/turtle")
         self.assertTrue('Location' in response.headers)
 
         nanopub = self.app.nanopub_manager.get(URIRef(response.headers['Location']))
@@ -31,71 +26,53 @@ class NanopubTest(ApiTestCase):
         self.assertEquals(len(nanopub.provenance), 0)
 
     def test_read(self):
-        self.login(*self.create_user("user@example.com","password"))
-        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle",follow_redirects=True)
-        self.assertEquals(response.status,'201 CREATED')
+        response = self.publish_nanopub(self.turtle, "text/turtle")
         self.assertTrue('Location' in response.headers)
-        
-        nanopub_id = response.headers['Location'].split('/')[-1]
-        
-        content = self.client.get("/pub/"+nanopub_id,headers={'Accept':'application/json'}, follow_redirects=True)
+
+        nanopub_id = response.headers['Location'].split('/')[-1]        
+        content = self.client.get("/pub/"+nanopub_id, headers={'Accept':'application/json'}, follow_redirects=True)
         g = ConjunctiveGraph()
         self.assertEquals(content.mimetype, "application/json")
-        g.parse(data=str(content.data,'utf8'), format="json-ld")
-        
+        g.parse(data=str(content.data, 'utf8'), format="json-ld")
+
         self.assertEquals(len(g), 17)
         self.assertEquals(g.value(URIRef('http://example.com/janedoe'), RDF.type), URIRef('http://schema.org/Person'))
 
     def test_delete_admin(self):
-        self.login(*self.create_user("user@example.com","password"))
-
-        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle",follow_redirects=True)
-        self.assertEquals(response.status,'201 CREATED')
+        response = self.publish_nanopub(self.turtle, "text/turtle")
         self.assertTrue('Location' in response.headers)
-        
+
         nanopub_id = response.headers['Location'].split('/')[-1]
         response = self.client.delete("/pub/"+nanopub_id, follow_redirects=True)
         self.assertEquals(response.status,'204 NO CONTENT')
 
     def test_delete_nonadmin(self):
-        self.login(*self.create_user("user@example.com","password",roles=None))
-
-        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle",follow_redirects=True)
-        self.assertEquals(response.status,'201 CREATED')
+        response = self.publish_nanopub(self.turtle, "text/turtle")
         self.assertTrue('Location' in response.headers)
-        
+
         nanopub_id = response.headers['Location'].split('/')[-1]
 
         response = self.client.delete("/pub/"+nanopub_id, follow_redirects=True)
-        self.assertEquals(response.status,'204 NO CONTENT')
+        self.assertEquals(response.status, '204 NO CONTENT')
 
     def test_linked_data(self):
-        self.login(*self.create_user("user@example.com","password"))
+        self.publish_nanopub(self.turtle, "text/turtle")
 
-        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle",follow_redirects=True)
-        
-        self.assertEquals(response.status,'201 CREATED')
-        
-        content = self.client.get("/about",query_string={"uri":"http://example.com/janedoe"},follow_redirects=True)
+        content = self.get_view("http://exmaple.com/janedoe", expected_mime="text/turtle")
+
         g = Graph()
-        self.assertEquals(content.mimetype, "text/turtle")
-        g.parse(data=str(content.data,'utf8'), format="turtle")
-        
+        g.parse(data=str(content.data, 'utf8'), format="turtle")
+
         self.assertEquals(len(g), 5)
         self.assertEquals(g.value(URIRef('http://example.com/janedoe'), RDF.type), URIRef('http://schema.org/Person'))
 
-        
     def test_attribute_view(self):
-        self.login(*self.create_user("user@example.com", "password"))
-        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle", follow_redirects=True)
-        
-        self.assertEquals(response.status,'201 CREATED')
-        
-        content = self.client.get("/about",query_string={"uri":"http://example.com/janedoe", 'view':'attributes'},follow_redirects=True)
-        
-        self.assertEquals(content.mimetype, "application/json")
-        json_content = json.loads(str(content.data, 'utf8'))
+        self.publish_nanopub(self.turtle, "text/turtle")
 
+        content = self.get_view("http://example.com/janedoe", view="attributes",
+                                expected_mime="application/json")
+
+        json_content = json.loads(str(content.data, 'utf8'))
         self.assertEquals(json_content['label'], "Jane Doe")
         self.assertEquals(len(json_content['type']), 1)
         self.assertEquals(json_content['type'][0]['label'], 'Person')
@@ -109,21 +86,10 @@ class NanopubTest(ApiTestCase):
         <http://schema.org/url> <http://www.janedoe.com> ;
         <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> . """
 
-        self.login(*self.create_user("user@example.com", "password"))
-        response = self.client.post("/pub",
-                                    data=ontology,
-                                    content_type="text/turtle",
-                                    follow_redirects=True)
-        self.assertStatus(response, 201,
-                          "Did not receive '201 CREATED' when publishing ontology nanopub")
-
-        content = self.client.get("/about",
-                                  query_string={"uri":"http://example.com/", 'view':'describe'},
-                                  follow_redirects=True)
-        # Needs blinker installed
-        self.assertTemplateUsed("describe_ontology.json")
-        self.assertEquals(content.mimetype, "application/json",
-                          "Did not receive JSON for 'describe' view")
+        self.publish_nanopub(ontology, "application/json")
+        content = self.get_view("http://example.com/", view="describe",
+                                expected_mime="application/json",
+                                expected_template="describe_ontology.json")
 
         data = json.loads(str(content.data, 'utf8'))
         self.assertIsInstance(data, list, "'Describe' view returned unexpected structure")
--- whyis/test/api_test_case.py
+++ whyis/test/api_test_case.py
@@ -2,4 +2,29 @@ from .test_case import TestCase
 
 
 class ApiTestCase(TestCase):
-    pass
+
+    def publish_nanopub(self, data, content_type, expected_response=201, login=True):
+        if login:
+            self.login(*self.create_user("user@example.com", "password"))
+
+        response = self.client.post("/pub", data=data,
+                                    content_type=content_type,
+                                    follow_redirects=True)
+        self.assertStatus(response, expected_response,
+                          "Expected {}, got {} as reponse".format(expected_response, response))
+
+        return response
+        
+    def get_view(self, uri, view="view", expected_mime=None, expected_template=None):
+        content = self.client.get("/about",
+                                query_string={"uri": uri, "view": view},
+                                follow_redirects=True)
+        
+        if expected_template is not None:
+            self.assertTemplateUsed(expected_template)
+
+        if expected_mime is not None:
+            self.assertEquals(content.mimetype, expected_mime,
+                              "Expected {}, got {} as response MIME type".format(expected_mime, content.mimetype))
+
+        return content
