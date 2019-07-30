fc4dc659906a438a7f74c322c71fe5832c33da07
Minor Gordon <gordom6@rpi.edu>
2019-07-30 17:48:39 -0400

    load app data correctly

--- docker/image/load-whyis-data.sh
+++ docker/image/load-whyis-data.sh
@@ -8,4 +8,16 @@ then
     curl -s -X POST --data-binary "@/apps/whyis/knowledge.properties" -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace > /data/knowledge_namespace.log
 
     echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > /data/.whyis_data_loaded
+    echo "Loaded whyis data"
+fi
+
+APPNAME=`find /apps -maxdepth 1 -type d -and -not -path /apps/whyis -and -not -path /apps -and -not -path '*/\.*' -exec basename \{} \;`
+
+if [ -f "/apps/${APPNAME}/load-data.sh" ] ;
+then
+    if [ ! -f "/data/.${APPNAME}_data_loaded" ] ;
+    then
+        sudo -i -u whyis /apps/${APPNAME}/load-data.sh
+        echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > "/data/.${APPNAME}_data_loaded"
+    fi
 fi
