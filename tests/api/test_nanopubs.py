8ab7a691e05b114517ca2cb28ba015ee47a3b318
Owen Xie <owenxie24@gmail.com>
2019-07-30 11:39:42 -0400

    Add test for ontology describe view

--- tests/api/test_nanopubs.py
+++ tests/api/test_nanopubs.py
@@ -13,8 +13,7 @@ class NanopubTest(ApiTestCase):
     <http://schema.org/telephone> "(425) 123-4567" ;
     <http://schema.org/url> <http://www.janedoe.com> ;
     <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .
-'''        
-
+'''
     
     def test_create(self):
         self.login(*self.create_user("user@example.com","password"))
@@ -87,8 +86,8 @@ class NanopubTest(ApiTestCase):
 
         
     def test_attribute_view(self):
-        self.login(*self.create_user("user@example.com","password"))
-        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle",follow_redirects=True)
+        self.login(*self.create_user("user@example.com", "password"))
+        response = self.client.post("/pub", data=self.turtle, content_type="text/turtle", follow_redirects=True)
         
         self.assertEquals(response.status,'201 CREATED')
         
@@ -100,3 +99,45 @@ class NanopubTest(ApiTestCase):
         self.assertEquals(json_content['label'], "Jane Doe")
         self.assertEquals(len(json_content['type']), 1)
         self.assertEquals(json_content['type'][0]['label'], 'Person')
+
+    def test_ontology_describe_view(self):
+        ontology = """
+        <http://example.com/> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#Ontology> .
+        <http://example.com/janedoe> <http://schema.org/jobTitle> "Professor";
+        <http://schema.org/name> "Jane Doe" ;
+        <http://schema.org/telephone> "(425) 123-4567" ;
+        <http://schema.org/url> <http://www.janedoe.com> ;
+        <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> . """
+
+        self.login(*self.create_user("user@example.com", "password"))
+        response = self.client.post("/pub",
+                                    data=ontology,
+                                    content_type="text/turtle",
+                                    follow_redirects=True)
+        self.assertStatus(response, 201,
+                          "Did not receive '201 CREATED' when publishing ontology nanopub")
+
+        content = self.client.get("/about",
+                                  query_string={"uri":"http://example.com/", 'view':'describe'},
+                                  follow_redirects=True)
+        # Needs blinker installed
+        self.assertTemplateUsed("describe_ontology.json")
+        self.assertEquals(content.mimetype, "application/json",
+                          "Did not receive JSON for 'describe' view")
+
+        data = json.loads(str(content.data, 'utf8'))
+        self.assertIsInstance(data, list, "'Describe' view returned unexpected structure")
+        self.assertTrue(data, "'Describe' view returned empty list")
+        self.assertIn("@graph", data[0], "'Describe' view missing @graph key")
+        self.assertIn("@id", data[0], "'Describe' view missing @id key")
+
+        graph = data[0]["@graph"]
+        self.assertTrue(graph, "'Describe' view returned no subjects")
+
+        for subject in graph:
+            if subject["@id"] == "http://example.com/":
+                self.assertEquals(len(subject.keys()), 2,
+                                  "Subject in 'describe' view has unexpected number of predicates")
+            elif subject["@id"] == "http://example.com/janedoe":
+                self.assertEquals(len(subject.keys()), 6,
+                                  "Subject in 'describe' view has unexpected number of predicates")
