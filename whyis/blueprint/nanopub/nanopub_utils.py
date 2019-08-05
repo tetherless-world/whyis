2d703ece84dacb3554202b365ea2cee3cebe1f96
Minor Gordon <gordom6@rpi.edu>
2019-08-05 16:27:00 -0400

    DATA_FORMATS

--- whyis/blueprint/nanopub/nanopub_utils.py
+++ whyis/blueprint/nanopub/nanopub_utils.py
@@ -2,6 +2,8 @@ from flask import current_app, request
 from rdflib import URIRef, ConjunctiveGraph
 import sadi.mimeparse
 
+from whyis.data_formats import DATA_FORMATS
+
 
 def get_nanopub_uri(ident):
     return URIRef('%s/pub/%s'%(current_app.config['lod_prefix'], ident))
@@ -12,7 +14,7 @@ def get_nanopub_graph():
     contentType = request.headers['Content-Type']
     encoding = 'utf8' if not request.content_encoding else request.content_encoding
     content = str(request.data, encoding)
-    fmt = sadi.mimeparse.best_match([mt for mt in list(current_app.dataFormats.keys()) if mt is not None],contentType)
-    if fmt in current_app.dataFormats:
-        inputGraph.parse(data=content, format=current_app.dataFormats[fmt])
+    fmt = sadi.mimeparse.best_match([mt for mt in list(DATA_FORMATS.keys()) if mt is not None],contentType)
+    if fmt in DATA_FORMATS:
+        inputGraph.parse(data=content, format=DATA_FORMATS[fmt])
     return inputGraph
