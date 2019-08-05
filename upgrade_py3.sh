600f625fbfa5d8c2049cad6a7f80ad09a577675e
Minor Gordon <gordom6@rpi.edu>
2019-08-05 13:48:06 -0400

    manifests/ -> puppet/manifests/

--- upgrade_py3.sh
+++ upgrade_py3.sh
@@ -18,6 +18,6 @@ sudo /opt/puppetlabs/bin/puppet module install richardc-datacat
 sudo /opt/puppetlabs/bin/puppet module install puppetlabs-java
 sudo /opt/puppetlabs/bin/puppet module install puppet-nodejs --version 7.0.1
 
-sudo /opt/puppetlabs/bin/puppet apply manifests/install.pp
+sudo /opt/puppetlabs/bin/puppet apply puppet/manifests/install.pp
 
 echo "To complete the upgrade, re-install your whyis app using 'pip install -e' into /apps/whyis/venv and restart apache2 and celeryd."
