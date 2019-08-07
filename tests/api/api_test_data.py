4883989c5cd36a4aa66dcaa647626a551b2b510d
Minor Gordon <gordom6@rpi.edu>
2019-08-07 18:08:23 -0400

    add more test data

--- tests/api/api_test_data.py
+++ tests/api/api_test_data.py
@@ -1,9 +1,16 @@
 HOME_INSTANCE_URI = "http://localhost:5000/Home"
 
+HOME_PAGE_CLASS_URI = "http://vocab.rpi.edu/whyis/HomePage"
+
 ONTOLOGY_INSTANCE_URI = "http://example.com/janetology"
 
 ONTOLOGY_INSTANCE_TURTLE = """\
 <%(ONTOLOGY_INSTANCE_URI)s> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#Ontology> .""" % globals()
+
+PERSON_CLASS_URI = "http://schema.org/Person"
+
+PERSON_CLASS_TURTLE = """\
+<%(PERSON_CLASS_URI)s> a <http://www.w3.org/2002/07/owl#Class> .""" % locals()
 
 PERSON_INSTANCE_URI = "http://example.com/janedoe"
 
