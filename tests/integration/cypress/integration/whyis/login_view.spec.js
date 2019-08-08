d6014856addccb8cf316a267d53f5e1de72b2a66
Minor Gordon <gordom6@rpi.edu>
2019-08-08 17:12:26 -0400

    log out from login view spec

--- tests/integration/cypress/integration/whyis/login_view.spec.js
+++ tests/integration/cypress/integration/whyis/login_view.spec.js
@@ -1,21 +1,24 @@
-context('login_view', () => {
-    before(() => {
-       cy.fixture("credentials").as("credentials");
+const login = () => {
+    cy.fixture("credentials").then((credentials) => {
+        cy.get("#email").type(credentials.email)
+        cy.get("#password").type(credentials.password);
+        cy.get("#remember").click();
+        cy.get("#submit").click();
+        cy.url().should("eq", Cypress.config().baseUrl + "/");
     });
+}
 
+context('login_view', () => {
     beforeEach(() => {
         cy.visit("/login");
     });
 
-    it ("should allow logging in with correct credentials", () => {
-        cy.get("@credentials").then((credentials) => {
-            cy.get("#email").type(credentials.email)
-            cy.get("#password").type(credentials.password);
-            cy.get("#remember").click();
-            cy.get("#submit").click();
-            cy.url().should("eq", Cypress.config().baseUrl + "/");
-        });
+    it ("should allow logging in with correct credentials", login);
+
+    it ("should allow logging out after logging in", () => {
+        login();
+        cy.get("#current-user-dropdown").click();
+        cy.get("#log-out").click();
+        cy.url().should("eq", Cypress.config().baseUrl + "/");
     });
 });
-
-
