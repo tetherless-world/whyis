c9bf0005642831410b44efb2314b01493d469174
Minor Gordon <gordom6@rpi.edu>
2019-08-09 17:48:04 -0400

    separate out integration testing

---
+++ .circleci/test-unit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+WHYIS_IMAGE="${1:-tetherlessworld/whyis:latest}"
+WHYIS_DEMO_IMAGE="tetherlessworld/whyis-demo:latest"
+
+mkdir -p test-results/py
+
+# docker run -e "CI=$CI" $WHYIS_IMAGE bash -c "python3 manage.py test"
+# docker run -e "CI=$CI" $WHYIS_IMAGE bash -c "python3 manage.py test --ci && cat test-results/py/results.xml"
+
+echo "`date` Running Python tests in $WHYIS_IMAGE"
+docker run -e "CI=$CI" $WHYIS_IMAGE bash -c "mkdir -p test-results/py && python3 manage.py test --ci >test-results/py/test.out 2>test-results/py/test.err; tar cf test-results-py.tar test-results/py && cat test-results-py.tar" >test-results-py.tar
+tar xf test-results-py.tar
+echo "`date` Python test stdout:"
+cat test-results/py/test.out
+echo "`date` Python test stderr:"
+cat test-results/py/test.err
+if [ ! -f "test-results/py/results.xml" ]; then
+    echo "Python test results.xml does not exist, exiting abnormally"
+    exit 1
+fi
+if [ "$(grep -c 'failure ' test-results/py/results.xml)" -ge 1 ]; then
+    echo "Python test results.xml has failures, exiting abnormally"
+    exit 1
+fi
+
+echo "`date` Exiting normally"
+exit 0
