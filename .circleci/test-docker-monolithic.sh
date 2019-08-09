26e18f064076a52101a6e3002f0837a84d24eca0
Minor Gordon <gordom6@rpi.edu>
2019-08-09 09:29:11 -0400

    print progress in test script

--- .circleci/test-docker-monolithic.sh
+++ .circleci/test-docker-monolithic.sh
@@ -4,30 +4,38 @@ WHYIS_IMAGE="${1:-tetherlessworld/whyis:latest}"
 
 mkdir -p test-results/py
 
+echo "Running Python tests in $WHYIS_IMAGE, will print to console"
 docker run -e "CI=$CI" $WHYIS_IMAGE bash -c "python3 manage.py test"
 
 # docker run -e "CI=$CI" $WHYIS_IMAGE bash -c "python3 manage.py test --ci && cat test-results/py/results.xml"
 
+echo "Running Python tests in $WHYIS_IMAGE, will output junit.xml and coverage reports"
 docker run -e "CI=$CI" $WHYIS_IMAGE bash -c "python3 manage.py test --ci &>/dev/null && tar cf test-results-py.tar test-results/py && cat test-results-py.tar" >test-results-py.tar
 
 tar xf test-results-py.tar
 
 if [ ! -f "test-results/py/results.xml" ]; then
+    echo "Python test results.xml does not exist, exiting abnormally"
     exit 1
 fi
 
+echo "Python test results.xml:"
 cat test-results/py/results.xml
 
 if [ "$(grep -c 'failure ' test-results/py/results.xml)" -ge 1 ]; then
+    echo "Python test results.xml has failures, exiting abnormally"
     exit 1
 fi
 
 mkdir -p test-results/js
 
+echo "Running integration tests in $WHYIS_IMAGE, will write junit.xml"
 docker run $WHYIS_IMAGE bash -c "curl -sL https://deb.nodesource.com/setup_12.x | bash - && apt-get install -y nodejs && mkdir -p test-results/js && cd tests/integration && npm install && npm run cypress:run-ci &>/dev/null && cat /apps/whyis/test-results/js/results.xml" >test-results/js/results.xml
 
 if [ "$(grep -c 'failure ' test-results/js/results.xml)" -ge 1 ]; then
+    echo "Integration test results.xml has failures, exiting abnormally"
     exit 1
 fi
 
+echo "Exiting normally"
 exit 0
