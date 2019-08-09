dbdbac4403e86a88dcf4fc30f0e26041f122db53
Minor Gordon <gordom6@rpi.edu>
2019-08-09 12:01:18 -0400

    fix path

--- .circleci/test-docker-monolithic.sh
+++ .circleci/test-docker-monolithic.sh
@@ -26,9 +26,13 @@ echo "Running integration tests in $WHYIS_DEMO_IMAGE"
 #JS_REDIRECT=""
 #docker run $WHYIS_DEMO_IMAGE bash -c "mkdir -p test-results/js && curl -sL https://deb.nodesource.com/setup_12.x | bash - $JS_REDIRECT && apt-get install -y nodejs xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 $JS_REDIRECT && cd tests/integration && npm install $JS_REDIRECT && CYPRESS_baseUrl=http://localhost npm run cypress:run $JS_REDIRECT"
 JS_REDIRECT=">/apps/whyis/test-results/js/test.out 2>/apps/whyis/test-results/js/test.err"
-docker run $WHYIS_DEMO_IMAGE bash -c "mkdir -p test-results/js && curl -sL https://deb.nodesource.com/setup_12.x | bash - $JS_REDIRECT && apt-get install -y nodejs xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 $JS_REDIRECT && cd tests/integration && npm install $JS_REDIRECT && CYPRESS_baseUrl=http://localhost npm run cypress:run-ci $JS_REDIRECT; tar cf test-results-js.tar test-results/js && cat test-results-js.tar" >test-results-js.tar
+docker run $WHYIS_DEMO_IMAGE bash -c "mkdir -p test-results/js && curl -sL https://deb.nodesource.com/setup_12.x | bash - $JS_REDIRECT && apt-get install -y nodejs xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 $JS_REDIRECT && cd tests/integration && npm install $JS_REDIRECT && CYPRESS_baseUrl=http://localhost npm run cypress:run-ci $JS_REDIRECT; cd /apps/whyis && tar cf test-results-js.tar test-results/js && cat test-results-js.tar" >test-results-js.tar
 tar xf test-results-js.tar
 cat test-results/js/test.out
+if [ ! -f "test-results/js/results.xml" ]; then
+    echo "Integration test results.xml does not exist, exiting abnormally"
+    exit 1
+fi
 if [ "$(grep -c 'failure ' test-results/js/results.xml)" -ge 1 ]; then
     echo "Integration test results.xml has failures, exiting abnormally"
     exit 1
