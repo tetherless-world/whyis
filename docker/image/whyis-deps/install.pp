c8f811fa412f8fea3f885e3ba65e4b8ef393c600
Owen Xie <owenxie24@gmail.com>
2019-08-12 15:24:10 -0400

    Upgrade monolithic container to 18.04

--- docker/image/whyis-deps/install.pp
+++ docker/image/whyis-deps/install.pp
@@ -2,7 +2,7 @@ Exec { path => ["/usr/local/sbin","/usr/local/bin","/usr/sbin","/usr/bin","/bin"
 
 # jdk is required to install javabridge
 package { ["unzip", "zip", "default-jdk", "build-essential", "automake", "jetty9", "subversion", "git",
-  "libapache2-mod-wsgi-py3", "libblas3", "libblas-dev", "celeryd", "redis-server", "apache2", "libffi-dev", "libssl-dev"
+  "libapache2-mod-wsgi-py3", "libblas3", "libblas-dev", "redis-server", "apache2", "libffi-dev", "libssl-dev"
   , "maven", "python3-dev", "python3-pip", "libdb5.3-dev"]:
   ensure => "installed"
 } ->
@@ -114,4 +114,10 @@ file { "/var/log/whyis":
   ensure => directory,
   owner => "whyis",
   group => "whyis",
+}
+
+# Register the generic celery daemon service
+file { "/etc/init.d/celeryd"
+  ensure => file,
+  source => "/apps/whyis/puppet/files/etc/init.d/celeryd"
 }
