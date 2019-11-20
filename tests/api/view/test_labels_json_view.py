6ff54febf32444b188dd51b3b6a4a9eb2254da23
Henrique O. Santos <hensantos@yahoo.com>
2019-11-20 12:28:12 -0500

    HOME_INSTANCE_URI from LOD_PREFIX

--- tests/api/view/test_labels_json_view.py
+++ tests/api/view/test_labels_json_view.py
@@ -1,10 +1,16 @@
-from ..api_test_data import HOME_INSTANCE_URI
 from whyis.test.api_test_case import ApiTestCase
 import json
 
 
 class TestLabelsJsonView(ApiTestCase):
     def test(self):
+        try:
+            import config
+        except:
+            from whyis import config_defaults as config
+        
+        HOME_INSTANCE_URI = config.LOD_PREFIX + "/Home"
+
         self.login_new_user()
 
         content = self.get_view(uri=HOME_INSTANCE_URI,
