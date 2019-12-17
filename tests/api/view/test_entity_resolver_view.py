4c1ffb8848f1a36916823c6dfafb0e439253c364
Peter Wood <woodp@rpi.edu>
2019-12-17 18:24:38 -0500

    test context parameter for entity resolver

--- tests/api/view/test_entity_resolver_view.py
+++ tests/api/view/test_entity_resolver_view.py
@@ -11,7 +11,8 @@ TEST_DATA_TURTLE = '''
 
 :centimeter a :UnitOfMeasure;
   skos:notation "cm";
-  rdfs:label "centimeter".
+  rdfs:label "centimeter";
+  skos:definition "Some text to provide context.".
 '''
 bds = Namespace("http://www.bigdata.com/rdf/search#")
 
@@ -56,6 +57,24 @@ class TestEntityResolverView(ApiTestCase):
         self.app.db.add((Literal("centimeter"), bds.matchAllTerms, Literal("false")))
         self.app.db.add((Literal("centimeter"), bds.relevance, Literal(1.0)))
         content = self.client.get("/?view=resolve&term=centi",
+                                  headers={'Accept':'application/json'},
+                                  follow_redirects=True)
+        results = json.loads(content.data.decode('utf8'))
+        self.assertEquals(len(results),1)
+        self.assertEquals(results[0]['node'], "http://example.com/centimeter")
+
+    def test_context_lookup(self):
+        self.login_new_user()
+        response = self.post_nanopub(data=TEST_DATA_TURTLE,
+                                     content_type="text/turtle",
+                                     expected_headers=["Location"])
+        self.app.db.add((Literal("centimeter"), bds.search, Literal("centimeter")))
+        self.app.db.add((Literal("centimeter"), bds.matchAllTerms, Literal("false")))
+        self.app.db.add((Literal("centimeter"), bds.relevance, Literal(1.0)))
+        self.app.db.add((Literal("Some text to provide context."), bds.search, Literal("context")))
+        self.app.db.add((Literal("Some text to provide context."), bds.matchAllTerms, Literal("false")))
+        self.app.db.add((Literal("Some text to provide context."), bds.relevance, Literal(1.0)))
+        content = self.client.get("/?view=resolve&term=centimeter&context=context",
                                   headers={'Accept':'application/json'},
                                   follow_redirects=True)
         results = json.loads(content.data.decode('utf8'))
