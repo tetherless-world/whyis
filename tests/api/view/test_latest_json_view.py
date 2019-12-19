4197c229e265a850882d79781522b99d2c7922f7
Peter Wood <woodp@rpi.edu>
2019-12-19 16:31:30 -0500

    Fleshed out 'latest' view test. Doesn't work yet though.

--- tests/api/view/test_latest_json_view.py
+++ tests/api/view/test_latest_json_view.py
@@ -2,6 +2,32 @@ from ..api_test_data import PERSON_INSTANCE_TURTLE
 from whyis.test.api_test_case import ApiTestCase
 import json
 
+some_nanopub="""@prefix : <http://www.example.org/whyis#> .
+@prefix np:  <http://www.nanopub.org/nschema#> .
+@prefix sio: <http://semanticscience.org/resource/> .
+
+    :npTest a np:Nanopublication .
+    :npTest np:hasAssertion :assertion .
+    :npTest np:hasProvenance :provenance .
+    :npTest np:hasPublicationInfo :pubInfo .
+
+:npTest {
+    :npTest sio:isAbout :JaneDoe .
+}
+
+:assertion {
+    :JaneDoe <http://schema.org/name> "Jane Doe".
+    :assertion a np:Assertion .
+}
+
+:provenance {
+    :provenance a np:Provenance .
+}
+
+:pubInfo {
+    :pubInfo a np:PublicationInfo .
+    :assertion <http://purl.org/dc/elements/1.1/created> "Something" .
+}"""
 
 class TestLatestJsonView(ApiTestCase):
     def test(self):
@@ -14,8 +40,8 @@ class TestLatestJsonView(ApiTestCase):
 
         self.login_new_user()
 
-        self.post_nanopub(data=PERSON_INSTANCE_TURTLE,
-                          content_type="text/turtle")
+        self.post_nanopub(data=some_nanopub,
+                          content_type="application/trig")
 
         content = self.get_view(uri=HOME_INSTANCE_URI,
                                 view="latest",
@@ -25,4 +51,6 @@ class TestLatestJsonView(ApiTestCase):
 
         json_content = json.loads(str(content.data, 'utf8'))
         self.assertIsInstance(json_content, list)
-        self.assertEquals(len(json_content), 0)
+#        self.assertEquals(len(json_content), 0)
+        print(json_content)
+        print(content)
