4b0c901fa4ad435cb7f72aa3cdb2934fc4dc9fff
aswallace <aswallace98@gmail.com>
2020-10-29 23:08:37 +0000

    New branch for fixing the md-autocomplete bug for dataset upload
    The md-autocomplete problem is in dataset-uploader.html around line 148.
    I used the version that we pasted in from the Vue Material documentaiton, but my original
    version is in md-autocomplete.html.

--- static/js/whyis_vue/components/pages/dataset/upload-new/dataset-uploader.vue
+++ static/js/whyis_vue/components/pages/dataset/upload-new/dataset-uploader.vue
@@ -12,11 +12,5 @@
         margin-top: 36px;
         display: block;
     }
-    .md-virtual-repeat-container.md-autocomplete-suggestions-container {
-        width:700px !important;
-    }  
-    .md-menu-content {
-        width: 1000px !important;
-    }
 </style>
 <script src="./dataset-uploader-script.js"></script>
