f01a8e0fe2c0ad36bd8154270e615752438f66ae
Tolulomo <tolulomofateye@gmail.com>
2020-11-09 12:53:04 -0500

    updates to remove and use parent component styling

--- static/js/whyis_vue/components/pages/dataset/upload-new/dataset-uploader.vue
+++ static/js/whyis_vue/components/pages/dataset/upload-new/dataset-uploader.vue
@@ -1,16 +1,2 @@
 <template src="./dataset-uploader.html"></template>
-<style scoped lang="scss" src="../../../../assets/css/main.scss"></style>
-<style type="text/css" scoped>
-    /** .md-menu-content-bottom-start {
-        `z-index:1000 !important; 
-        width: 100% !important;  
-        position: absolute;`
-    } */
-</style>
-<style lang="scss" scoped>
-    .md-autocomplete + strong {
-        margin-top: 36px;
-        display: block;
-    }
-</style>
 <script src="./dataset-uploader-script.js"></script>
