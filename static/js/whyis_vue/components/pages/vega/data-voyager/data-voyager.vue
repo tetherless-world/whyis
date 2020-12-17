3b4b5b753fc086b9955eea6882365bde83d73942
Sam Stouffer <samuellstouffer@gmail.com>
2020-12-17 12:20:49 -0500

    data voyager integration WIP

---
+++ static/js/whyis_vue/components/pages/vega/data-voyager/data-voyager.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="voyager-embed"></div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import { CreateVoyager } from 'datavoyager'
+  // import 'datavoyager/dist/static/js/main.21331228'
+  import 'datavoyager/dist/static/css/main.a907da23.css'
+
+  export default {
+    props: {
+      data: {
+        type: Object,
+        default: () => null,
+      }
+    },
+    mounted() {
+      console.log('data', this.data)
+      const container = document.getElementById("voyager-embed");
+      const config = undefined;
+      const data = this.data;
+      // const data = {
+      //   "values": [
+      //     {"fieldA": "A", "fieldB": 28}, {"fieldA": "B", "fieldB": 55}, {"fieldA": "C", "fieldB": 43},
+      //     {"fieldA": "D", "fieldB": 91}, {"fieldA": "E", "fieldB": 81}, {"fieldA": "F", "fieldB": 53},
+      //     {"fieldA": "G", "fieldB": 19}, {"fieldA": "H", "fieldB": 87}, {"fieldA": "I", "fieldB": 52}
+      //   ]
+      // };
+      const voyagerInstance = CreateVoyager(container, config, data)
+    }
+  };
+</script>
+
+<style scoped>
+  #voyager-embed {
+    position: fixed;
+    background: #fafafa;
+    top:0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    z-index: 10;
+
+  }
+</style>
--- static/js/whyis_vue/components/pages/vega/view/vega-viewer.vue
+++ static/js/whyis_vue/components/pages/vega/view/vega-viewer.vue
@@ -26,6 +26,12 @@
             <md-icon>integration_instructions</md-icon>
           </md-button>
         </div>
+        <div>
+          <md-button class="md-icon-button" @click.native.prevent="openVoyager()">
+            <md-tooltip class="utility-bckg" md-direction="bottom"> View Data in Voyager </md-tooltip>
+            <md-icon>dynamic_form</md-icon>
+          </md-button>
+        </div>
         <div v-if="allowEdit">
           <md-button class="md-icon-button" @click.native.prevent="editChart">
             <md-tooltip class="utility-bckg" md-direction="top"> Edit Chart </md-tooltip>
@@ -91,11 +97,21 @@
           <md-button class="md-primary" @click="specViewer.show = false">Close</md-button>
         </md-dialog-actions>
       </md-dialog>
+      <!-- <md-dialog :md-active.sync="voyager.show">
+        <md-dialog-title>Viewing Data in Voyager</md-dialog-title>
+        <md-content class="voyager-view" v-if="voyager.show">
+        </md-content>
+        <md-dialog-actions>
+          <md-button class="md-primary" @click="voyager.show = false">Close</md-button>
+        </md-dialog-actions>
+      </md-dialog> -->
+      <data-voyager v-if="voyager.show" :data="spec.data"></data-voyager>
     </div>
   </div>
 </template>
 <style scoped lang="scss" src="../../../../assets/css/main.scss"></style>
 <style scoped lang="scss">
+
   .vega-spec-container {
     padding-left: 20px;
     padding-right: 20px;
@@ -108,15 +124,25 @@
     padding-left: 20px;
     padding-right: 20px;
   }
+  .voyager-view {
+    padding-left: 20px;
+    padding-right: 20px;
+    height: 80vh;
+    max-height: 100%;
+    width: 80vw;
+    max-width: 100%;
+  }
 </style>
 <script>
   import Vue from 'vue'
   import VJsoneditor from 'v-jsoneditor'
+
   import { EventServices, Slug } from '../../../../modules'
   import tempFiller from '../../../utils/temporary_filler'
   import { loadChart, buildSparqlSpec } from '../../../../utilities/vega-chart'
   import { querySparql } from '../../../../utilities/sparql'
   import { goToView } from '../../../../utilities/views'
+
   export default Vue.component('vega-viewer', {
     data() {
       return {
@@ -130,6 +156,9 @@
         authenticated: EventServices.authUser,
         allowEdit: false,
         vizOfTheDay: false,
+        voyager: {
+          show: false
+        },
         specViewer: {
           show: false,
           includeData: false,
@@ -168,6 +197,9 @@
           EventServices.toggleVizOfTheDay(args)
         }
         return EventServices.navTo('view', true)
+      },
+      openVoyager() {
+        this.voyager.show = true
       },
       shareChart() {
         return EventServices.$emit("dialoguebox", {status: true, share: true,
