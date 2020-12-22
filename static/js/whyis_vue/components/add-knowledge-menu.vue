b3b668c6faccebfcb5823dd3a8f53a73f531a8f4
Jim McCusker <mccusker@gmail.com>
2020-12-22 11:13:16 -0500

    added incoming links to the resource_view page, refactored the card layout, and have the menu added, but the CSS is wrong

---
+++ static/js/whyis_vue/components/add-knowledge-menu.vue
@@ -0,0 +1,20 @@
+<template>
+<md-menu md-size="medium" md-align-trigger>
+  <md-button md-menu-trigger><md-icon>menu</md-icon></md-button>
+
+  <md-menu-content>
+    <md-menu-item><add-link uri="uri"></add-link></md-menu-item>
+    <md-menu-item><add-type uri="uri"></add-type></md-menu-item>
+    <md-menu-item><add-attribute uri="uri"></add-attribute></md-menu-item>
+  </md-menu-content>
+</md-menu>
+</template>
+<style scoped lang="scss" src="../assets/css/main.scss"></style>
+<script>
+import Vue from "vue";
+
+export default Vue.component('add-knowledge-menu', {
+    props: ['uri']
+});
+
+</script>
--- static/js/whyis_vue/components/index.js
+++ static/js/whyis_vue/components/index.js
@@ -12,3 +12,4 @@ import './gallery';
 import './add-type';
 import './add-attribute';
 import './add-link';
+import './add-knowledge-menu';
