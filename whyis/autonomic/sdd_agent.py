c5103dbd67d6840d521b36d925d733dbd4977f9b
Jim McCusker <mccusker@gmail.com>
2021-01-05 19:08:31 -0500

    SDD2rdf now working as an agent.

--- whyis/autonomic/sdd_agent.py
+++ whyis/autonomic/sdd_agent.py
@@ -39,33 +39,47 @@ class SDDAgent(GlobalChangeService):
 
     def process(self, i, o):
         values = self.app.db.query('''prefix sio: <http://semanticscience.org/resource/>
-prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-prefix example-kb: <http://example.com/kb/example/> 
-prefix sdd: <http://purl.org/twc/sdd/> 
-prefix ov: <http://open.vocab.org/terms/> 
-prefix void: <http://rdfs.org/ns/void#> 
-prefix dcterms: <http://purl.org/dc/terms/> 
-prefix dcat: <http://www.w3.org/ns/dcat#> 
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix sdd: <http://purl.org/twc/sdd/>
+prefix ov: <http://open.vocab.org/terms/>
+prefix void: <http://rdfs.org/ns/void#>
+prefix dcterms: <http://purl.org/dc/terms/>
+prefix dcat: <http://www.w3.org/ns/dcat#>
 prefix csvw: <http://www.w3.org/ns/csvw#>
 prefix whyis: <http://vocab.rpi.edu/whyis/>
+prefix setl: <http://purl.org/twc/vocab/setl/>
+prefix prov:          <http://www.w3.org/ns/prov#>
 
 SELECT ?prefix ?sdd_file ?data_file ?content_type ?delimiter
 WHERE {
-?sdd_file rdf:type sdd:SemanticDataDictionary .
+    ?data_file dcterms:conformsTo ?sdd_file ;
+       ov:hasContentType ?content_type ;
+       csvw:delimiter ?delimiter .
 
-?data_file dcterms:conformsTo ?sdd_file ;
-    ov:hasContentType ?content_type ;
-    csvw:delimiter ?delimiter .
-
-?dataset dcat:distribution ?data_file ;
-    void:uriSpace ?prefix .
-FILTER NOT EXISTS { ?data_file rdf:type whyis:ProcessedFile . }
+   ?dataset dcat:distribution ?data_file ;
+       void:uriSpace ?prefix .
+    MINUS {
+        ?setl_script prov:wasDerivedFrom ?sdd_file;
+            a setl:SemanticETLScript.
+        ?dataset prov:wasGeneratedBy ?setl_script;
+            prov:wasGeneratedBy ?template.
+        ?template a setl:Transform;
+            prov:used/prov:wasGeneratedBy ?extract.
+        ?extract a setl:Extract;
+            prov:used ?data_file.
+    }
 }
-''')
-        output = sdd2setl(values.json["results"]["bindings"][0]["sdd_file"]["value"] + "#InfoSheet",values.json["results"]["bindings"][0]["prefix"]["value"],values.json["results"]["bindings"][0]["data_file"]["value"],values.json["results"]["bindings"][0]["content_type"]["value"],values.json["results"]["bindings"][0]["delimiter"]["value"],'Infosheet')
-        g = rdflib.Graph()
-        g.parse(data=output, format="turtle")
-        npub = Nanopublication(store=self.app.db.store)
-        for s, p, o in g:
-            npub.assertion.add((s, p, o))
-        npub.assertion.add((rdflib.URIRef(values.json["results"]["bindings"][0]["data_file"]["value"]), rdflib.RDF.type, whyis.ProcessedFile))
+''', initBindings={"sdd_file": i.identifier})
+        for prefix, sdd_file, data_file, content_type, delimiter in values:
+            output = sdd2setl(sdd_file + "#InfoSheet",
+                              prefix.value,
+                              data_file,
+                              content_type.value,
+                              delimiter.value)
+            npub = Nanopublication()
+            npub.assertion.parse(data=output, format="turtle")
+
+            sdd_setl = npub.assertion.value(predicate=rdflib.RDF.type, object=setl.SemanticETLScript)
+            npub.assertion.add((sdd_setl, prov.wasDerivedFrom, sdd_file))
+            new_nps = self.app.nanopub_manager.prepare(npub)
+            self.app.nanopub_manager.publish(*new_nps)
