cd72887c98142f4f8547fea8b91f5199344eee47
Jamie McCusker <mccusker@gmail.com>
2021-01-27 13:07:09 -0500

    Updated to python 3.7

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ WHYIS_BRANCH="${WHYIS_BRANCH:-release}"
 
 curl -s -O https://apt.puppetlabs.com/puppet-release-xenial.deb
 sudo dpkg -i puppet-release-xenial.deb
+
+sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 
 echo "Installing puppet..."
