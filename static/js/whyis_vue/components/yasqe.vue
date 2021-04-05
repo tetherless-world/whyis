1dc59b23c10740f1e8da86bcf2f62348bfd6e07c
Sam Stouffer <samuellstouffer@gmail.com>
2021-04-05 15:50:36 -0400

    modular viz editor WIP

--- static/js/whyis_vue/components/yasqe.vue
+++ static/js/whyis_vue/components/yasqe.vue
@@ -5,56 +5,50 @@
 <script>
 import Vue from "vue";
 
-export default Vue.component("yasqe", {
-  props: {
-    value: {
-      type: String,
-      default: () => ""
-    },
-    endpoint: {
-      type: String,
-      default: () => "/sparql"
-    },
-    showBtns: {
-      type: Boolean,
-      default: false
-    },
-    readOnly: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data() {
-    return {
-      editorValue: this.value
-    };
-  },
-  mounted() {
-    const yasqeContext = this;
-    this.yasqe = window.YASQE(this.$el, {
-      persistent: null,
-      sparql: {
-        showQueryButton: !this.showBtns,
-        endpoint: this.endpoint,
-        requestMethod: "POST",
-        callbacks: {
-          error() {
-            console.error("YASQE query error", arguments);
-            yasqeContext.$emit('query-error', error)
-          },
-          success(resp) {
-            yasqeContext.$emit("query-success", resp);
-          }
+export default Vue.component('yasqe', {
+    props: {
+        value: {
+            type: String,
+            default: () => ""
+        },
+        endpoint: {
+            type: String,
+            default: () => "/sparql"
+        },
+        showBtns: {
+            type: Boolean,
+            default: false
+        },
+        readOnly: {
+            type: Boolean,
+            default: false
         }
-      },
-      readOnly: this.readOnly
-    });
-    this.yasqe.setValue(this.value);
-    this.yasqe.on("changes", () => {
-      this.editorValue = yasqeContext.yasqe.getValue();
-      yasqeContext.$emit("input", this.editorValue);
-    });
-    this.yasqe.setSize("100%", "100%");
+    },
+    mounted () {
+        const yasqeContext = this
+        this.yasqe = window.YASQE(this.$el, {
+            persistent: null,
+            sparql: {
+                showQueryButton: !this.showBtns,
+                endpoint: this.endpoint,
+                requestMethod: "POST",
+                callbacks: {
+                    error () {
+                        console.error('YASQE query error', arguments)
+                    },
+                    success (resp) {
+                        yasqeContext.$emit('query-success', resp)
+                    },
+                }
+            },
+            readOnly: this.readOnly,
+        })
+        this.yasqe.setValue(this.value)
+        this.yasqe.on('changes', () => {
+            console.log('eyyy')
+            yasqeContext.$emit('input', yasqeContext.yasqe.getValue())
+        })
+        this.yasqe.setSize("100%", "100%")
   },
   watch: {
     value(value) {
