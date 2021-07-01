7439a39a600e237014f6fd2df1d1f95b844fbd35
Sabbir <rashidsabbir@gmail.com>
2021-07-01 06:44:12 +0000

    Updates to make abductor cmd work

--- whyis/commands/run_hybrid_reasoner.py
+++ whyis/commands/run_hybrid_reasoner.py
@@ -10,13 +10,13 @@ class RunHybridReasoner(Command):
         return [
             Option('--input', '-i', dest='reasoning_dict', required=True,
                    type=str),
-            Option('--mode', '-m', dest='mode', help='Select reasoning mode of either deductor or abductor', required=True,
+            Option('--mode', '-m', dest='mode', help='Select reasoning mode (either deductor or abductor)', required=True,
                    type=str),
         ]
 
     def run(self, reasoning_dict, mode):
+        npub = Nanopublication(store=current_app.db.store)
         if mode == 'deductor' :
-            npub = Nanopublication(store=current_app.db.store)
             if "active_profiles" and "reasoning_profiles" in current_app.config :
                 for profile in current_app.config["active_profiles"] :
                     for rule_reference in current_app.config["reasoning_profiles"][profile] :
@@ -34,29 +34,25 @@ class RunHybridReasoner(Command):
                         print(rule_reference)
                         for rule in current_app.config[reasoning_dict] :
                             if current_app.config[reasoning_dict][rule]["reference"] == rule_reference :
-                                #triples = current_app.db.query('''CONSTRUCT { %s } WHERE { %s } ''' % ( current_app.config[reasoning_dict][rule]["consequent"], current_app.config[reasoning_dict][rule]["antecedent"]), initNs=current_app.config[reasoning_dict][rule]["prefixes"] )
-                                print(current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"], current_app.config[reasoning_dict][rule]["antecedent"], current_app.config[reasoning_dict][rule]["consequent"], current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"], current_app.config[reasoning_dict][rule]["prefixes"])
                                 triples = current_app.db.query('''
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX sets: <http://purl.org/ontology/sets/ont#>
     CONSTRUCT {
-        ?g2 sets:hypothesis
-            [ a sets:Hypothesis , %s ; 
+        ?g2 <http://purl.org/ontology/sets/ont#hypothesis>
+            [ a <http://purl.org/ontology/sets/ont#Hypothesis> , %s ; 
                 rdfs:label "%s" ; 
-                sets:antecedentGraph ?g1 
+                <http://purl.org/ontology/sets/ont#antecedentGraph> ?g1 
             ]  .
     } WHERE {
         GRAPH ?g1 { %s }
         GRAPH ?g2 { %s }
         FILTER NOT EXISTS {
-            ?g2 sets:hypothesis 
-                [ a sets:Hypothesis , %s ; 
+            ?g2 <http://purl.org/ontology/sets/ont#hypothesis>
+                [ a <http://purl.org/ontology/sets/ont#Hypothesis> , %s ; 
                     rdfs:label "%s" ; 
-                    sets:antecedentGraph ?g1 
+                    <http://purl.org/ontology/sets/ont#antecedentGraph> ?g1 
                 ] .
         }
-    }''' % ( current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"], current_app.config[reasoning_dict][rule]["antecedent"], current_app.config[reasoning_dict][rule]["consequent"], current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"]), initNs=current_app.config[reasoning_dict][rule]["prefixes"] )
+    }'''% ( current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"], current_app.config[reasoning_dict][rule]["antecedent"], current_app.config[reasoning_dict][rule]["consequent"], current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"]), initNs=current_app.config[reasoning_dict][rule]["prefixes"] )
                                 for s, p, o, c in triples :
-                                    print(s, p, o)
+                                    print("Hybrid reasoner abductor adding: ", s, p, o)
+                                    npub.assertion.add((s, p, o))
 
