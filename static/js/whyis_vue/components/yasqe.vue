b06dd66e6dc10485ce31ec9be6f6d418edb802a9
aswallace <aswallace98@gmail.com>
2021-09-08 23:37:27 -0700

    Undo changes to yasqe

--- static/js/whyis_vue/components/yasqe.vue
+++ static/js/whyis_vue/components/yasqe.vue
@@ -4,57 +4,56 @@
 
 <script>
 import Vue from "vue";
-
-export default Vue.component('yasqe', {
-    props: {
-        value: {
-            type: String,
-            default: () => ""
-        },
-        endpoint: {
-            type: String,
-            default: () => "/sparql"
-        },
-        showBtns: {
-            type: Boolean,
-            default: false
-        },
-        readOnly: {
-            type: Boolean,
-            default: false
-        }
+export default Vue.component("yasqe", {
+  props: {
+    value: {
+      type: String,
+      default: () => ""
     },
-    data() {
-        return {
-            editorValue: this.value
-        }
+    endpoint: {
+      type: String,
+      default: () => "/sparql"
     },
-    mounted () {
-        const yasqeContext = this
-        this.yasqe = window.YASQE(this.$el, {
-            persistent: null,
-            sparql: {
-                showQueryButton: !this.showBtns,
-                endpoint: this.endpoint,
-                requestMethod: "POST",
-                callbacks: {
-                    error (error) {
-                        console.error('YASQE query error', error)
-                        yasqeContext.$emit('query-error', error)
-                    },
-                    success (resp) {
-                        yasqeContext.$emit('query-success', resp)
-                    },
-                }
-            },
-            readOnly: this.readOnly,
-        })
-        this.yasqe.setValue(this.value)
-        this.yasqe.on('changes', () => {
-            this.editorValue = yasqeContext.yasqe.getValue()
-            yasqeContext.$emit('input', this.editorValue)
-        })
-        this.yasqe.setSize("100%", "100%")
+    showBtns: {
+      type: Boolean,
+      default: false
+    },
+    readOnly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      editorValue: this.value
+    };
+  },
+  mounted() {
+    const yasqeContext = this;
+    this.yasqe = window.YASQE(this.$el, {
+      persistent: null,
+      sparql: {
+        showQueryButton: !this.showBtns,
+        endpoint: this.endpoint,
+        requestMethod: "POST",
+        callbacks: {
+          error() {
+            console.error("YASQE query error", arguments);
+            yasqeContext.$emit('query-error', error)
+          },
+          success(resp) {
+            yasqeContext.$emit("query-success", resp);
+          }
+        }
+      },
+      readOnly: this.readOnly
+    });
+    this.yasqe.setValue(this.value);
+    this.yasqe.on("changes", () => {
+      this.editorValue = yasqeContext.yasqe.getValue();
+      yasqeContext.$emit("input", this.editorValue);
+    });
+    this.yasqe.setSize("100%", "100%");
   },
   watch: {
     value(value) {
