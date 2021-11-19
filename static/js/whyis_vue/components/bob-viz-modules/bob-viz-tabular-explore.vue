b0c1362e1f993e46f9cc096d36441a232e87db9f
aswallace <aswallace98@gmail.com>
2021-11-19 05:43:29 -0800

    Prepare bobviz for merge to QA
    -Make the bobviz explore tabs more modular
    
    -Hard-code the sub-class and super-class elements in class view
    
    -Remove add-knowledge buttons from class view and resource view

--- static/js/whyis_vue/components/bob-viz-modules/bob-viz-tabular-explore.vue
+++ static/js/whyis_vue/components/bob-viz-modules/bob-viz-tabular-explore.vue
@@ -1,26 +1,26 @@
 <template>
 <div class="">
     <div class="viz-editor-tabs" @click="showTabNavigation">
-        <div class="viz-editor-tabs-item" id="tableResultsE" @click="tabNavigation" :style="showAllTabBtn ? showAllTabs : false">
+        <div v-if="tableResults.show" class="viz-editor-tabs-item" id="tableResults" @click="tabNavigation" :style="showAllTabBtn ? showAllTabs : false">
         Table
         <md-tooltip md-direction="bottom">View results as table</md-tooltip>
         </div>
-        <div class="viz-editor-tabs-item" id="vegaE" @click="tabNavigation" :style="showAllTabBtn ? showAllTabs : false">
+        <div v-if="vegaLite.show" class="viz-editor-tabs-item" id="vegaLite" @click="tabNavigation" :style="showAllTabBtn ? showAllTabs : false">
         Vega
         <md-tooltip md-direction="bottom">Enter Vega Specs</md-tooltip>
         </div>
-        <div class="viz-editor-tabs-item tabselected" id="dataVoyagerE" @click="tabNavigation" :style="showAllTabBtn ? showAllTabs : false">
+        <div v-if="dataVoyager.show" class="viz-editor-tabs-item" id="dataVoyager" @click="tabNavigation" :style="showAllTabBtn ? showAllTabs : false">
         Data Voyager
         <md-tooltip md-direction="bottom">Explore with Data Voyager</md-tooltip>
         </div>
     </div>
-     <div class="viz-editor" id="tableResultsc">
+     <div v-if="tableResults.show" class="viz-editor" id="tableResultsC">
         <bob-viz-results/>
      </div>
-     <div class="viz-editor" id="vegac">
+     <div v-if="vegaLite.show" class="viz-editor" id="vegaLiteC">
         <bob-viz-vega/>
      </div>
-     <div class="viz-editor viz-editor-show" id="dataVoyagerc">
+     <div v-if="dataVoyager.show" class="viz-editor" id="dataVoyagerC">
         <bob-viz-voyager/>
      </div>
 </div>
@@ -28,18 +28,32 @@
 <style scoped lang="scss" src="../../assets/css/main.scss"></style>
 <script>
 import Vue from 'vue';
-
-import { mapGetters } from 'vuex';
+ 
 export default Vue.component('bob-viz-tabular-explore', {
+  props: {
+    component_array: {
+      type: Array,
+      default: () => ['tableResults', 'vegaLite', 'dataVoyager']
+    },
+  },
   data() {
     return {
+      tableResults: {
+        show: false,
+        compId: 'tableResultsC',
+      },
+      vegaLite: {
+        show: false,
+        compId: 'vegaLiteC',
+      },
+      dataVoyager: {
+        show: false,
+        compId: 'dataVoyagerC',
+      },
       showAllTabBtn: false,
       showAllTabs: {display: 'none'},
     }
-  },
-  computed: {
-    ...mapGetters('bobViz', ['results', 'vegaData'])
-  },
+  }, 
   methods: {
     resize(e){
       if(e <= 26){
@@ -48,35 +62,44 @@ export default Vue.component('bob-viz-tabular-explore', {
         this.showAllTabBtn = false;
       }
     },
-  showTabNavigation(){
+    showTabNavigation(){
       this.paneResize = 50;
       this.showAllTabBtn = false;
       return this.paneResize = 18;
     },
     async tabNavigation(e){
-      const tableResults = document.getElementById('tableResultsc')
-      const vega = document.getElementById('vegac')
-      const dataVoyager = document.getElementById('dataVoyagerc')
       const tabs = await document.querySelectorAll('.viz-editor-tabs-item')
       if(tabs.length){
         [].forEach.call(tabs, function(el) {
             el.classList.remove('tabselected')})
       }
       e.srcElement.classList.add('tabselected')
-      if(e.srcElement.id == 'vegaE'){
-        tableResults.classList.remove('viz-editor-show')
-        dataVoyager.classList.remove('viz-editor-show')
-        vega.classList.add('viz-editor-show')
-      } else if(e.srcElement.id == 'dataVoyagerE') {
-        tableResults.classList.remove('viz-editor-show')
-        vega.classList.remove('viz-editor-show')
-        dataVoyager.classList.add('viz-editor-show')
-      } else {
-        vega.classList.remove('viz-editor-show')
-        dataVoyager.classList.remove('viz-editor-show')
-        tableResults.classList.add('viz-editor-show')
+      const components = await document.querySelectorAll('.viz-editor')
+      if(components.length){
+        [].forEach.call(components, function(el) {
+            el.classList.remove('viz-editor-show')})
+      }
+      if (this[e.srcElement.id]){
+        const showComponent = document.getElementById(this[e.srcElement.id]['compId'])
+        showComponent.classList.add('viz-editor-show')
       }
     },
+  },
+  created: function () {
+    let this_vue = this
+    if(this_vue.component_array.length){
+      [].forEach.call(this_vue.component_array, function(el) {
+        if (this_vue[el]){
+          this_vue[el]['show'] = true}
+        }) 
+    }
+  },
+  mounted: function () {
+    let showId = this.component_array[0]
+    let showTab = document.getElementById(showId)
+    showTab.classList.add('tabselected')
+    let showComponent = document.getElementById(this[showId]['compId'])
+    showComponent.classList.add('viz-editor-show')
   }
 })
 </script>
