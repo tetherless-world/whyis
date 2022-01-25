c68b861d62fdb6c5fe2bd352ddebb8afeb80feb1
Sabbir <rashidsabbir@gmail.com>
2022-01-25 02:58:17 +0000

    Initial updates for testing the running of aaa reasoner

--- whyis/commands/run_aaa_abductive_reasoner.py
+++ whyis/commands/run_aaa_abductive_reasoner.py
@@ -2,7 +2,7 @@ from flask import url_for, current_app
 from flask_script import Command, Option
 from nanopub import Nanopublication
 import urllib.request, urllib.parse, urllib.error
-
+import os
 
 class RunAAAReasoner(Command):
     """Display all valid routes in the application"""
@@ -16,43 +16,11 @@ class RunAAAReasoner(Command):
 
     def run(self, reasoning_dict, mode):
         npub = Nanopublication(store=current_app.db.store)
-        if mode == 'deductor' :
-            if "active_profiles" and "reasoning_profiles" in current_app.config :
-                for profile in current_app.config["active_profiles"] :
-                    for rule_reference in current_app.config["reasoning_profiles"][profile] :
-                        for rule in current_app.config[reasoning_dict] :
-                            if current_app.config[reasoning_dict][rule]["reference"] == rule_reference :
-                                print(rule_reference)
-                                triples = current_app.db.query('''CONSTRUCT { %s } WHERE { %s FILTER NOT EXISTS { %s } } ''' % ( current_app.config[reasoning_dict][rule]["consequent"], current_app.config[reasoning_dict][rule]["antecedent"], current_app.config[reasoning_dict][rule]["consequent"]), initNs=current_app.config[reasoning_dict][rule]["prefixes"] )
-                                for s, p, o, c in triples :
-                                    print("Hybrid reasoner deductor adding: ", s, p, o)
-                                    npub.assertion.add((s, p, o))
-        elif mode == 'abductor':
-            if "active_profiles" and "reasoning_profiles" in current_app.config :
-                for profile in current_app.config["active_profiles"] :
-                    for rule_reference in current_app.config["reasoning_profiles"][profile] :
-                        print(rule_reference)
-                        for rule in current_app.config[reasoning_dict] :
-                            if current_app.config[reasoning_dict][rule]["reference"] == rule_reference :
-                                triples = current_app.db.query('''
-    CONSTRUCT {
-        ?g2 <http://purl.org/ontology/sets/ont#hypothesis>
-            [ a <http://purl.org/ontology/sets/ont#Hypothesis> , %s ; 
-                rdfs:label "%s" ; 
-                <http://purl.org/ontology/sets/ont#antecedentGraph> ?g1 
-            ]  .
-    } WHERE {
-        GRAPH ?g1 { %s }
-        GRAPH ?g2 { %s }
-        FILTER NOT EXISTS {
-            ?g2 <http://purl.org/ontology/sets/ont#hypothesis>
-                [ a <http://purl.org/ontology/sets/ont#Hypothesis> , %s ; 
-                    rdfs:label "%s" ; 
-                    <http://purl.org/ontology/sets/ont#antecedentGraph> ?g1 
-                ] .
-        }
-    }'''% ( current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"], current_app.config[reasoning_dict][rule]["antecedent"], current_app.config[reasoning_dict][rule]["consequent"], current_app.config[reasoning_dict][rule]["rule"], current_app.config[reasoning_dict][rule]["reference"]), initNs=current_app.config[reasoning_dict][rule]["prefixes"] )
-                                for s, p, o, c in triples :
-                                    print("Hybrid reasoner abductor adding: ", s, p, o)
-                                    npub.assertion.add((s, p, o))
-
+        abducibles = "\"http://purl.org/twc/HEALS/ont/ClinicallyRelevantWeightGainPatient, http://purl.org/twc/HEALS/kb/jane, http://purl.org/twc/HEALS/ont/WeightGainPatient, http://purl.org/twc/HEALS/ont/ClinicallyRelevantWeightGain, http://purl.org/twc/HEALS/ont/Patient, http://semanticscience.org/resource/hasAttribute, http://purl.org/twc/HEALS/kb/clinicalWeightGain, http://purl.org/twc/HEALS/kb/clinicalBMIGain, http://purl.org/twc/HEALS/kb/biologicalEffect,	http://purl.org/twc/HEALS/kb/metabolismDecrease, http://purl.org/twc/HEALS/kb/hypothyroidism\""
+        depth = "3"
+        input_file = "examples/abductive_example.ttl" 
+        observation = "\"http://purl.org/twc/HEALS/kb/jane: http://purl.org/twc/HEALS/ont/ClinicallyRelevantWeightGainPatient\"" 
+        output_file = "output.txt" #java -Xmx4096m
+        abductor_command = "java -jar jars/AAA.jar -abd " + abducibles + " -d " + depth + " -i " + input_file + " -l -obs " + observation + " -out " + output_file + "-r"
+        print(abductor_command)
+        os.system(abductor_command)
